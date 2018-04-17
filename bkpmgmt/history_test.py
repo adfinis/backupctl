e74b3fa76e54bd3ffb28ee07d9c5c13a61fe3444
Tobias Rueetschi <tobias.ruetschi@adfinis-sygroup.ch>
2018-04-17 17:17:54 +0200

    create test cases for class history

---
+++ bkpmgmt/history_test.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+"""Test for class history"""
+
+import pytest
+
+from bkpmgmt import history
+
+
+@pytest.fixture(autouse=True)
+def hist():
+    hist_obj = history.History('/tmp/bkpmgmt.db')
+    return hist_obj
+
+
+def test_add_customer():
+    assert hist().add(
+        customer='customer1',
+        size='10G',
+        command='test',
+    )
+
+
+def test_add_vault():
+    assert hist().add(
+        customer='customer1',
+        vault='www.example.com',
+        size='10G',
+        command='test',
+    )
+
+
+def test_show_one():
+    hist().add(
+        customer='customer1',
+        size='10G',
+        command='test',
+    )
+    hist().add(
+        customer='customer2',
+        size='20G',
+        command='test',
+    )
+    tdata = hist().show(
+        count=1
+    )
+    assert type(tdata) == list
+    assert len(tdata) == 1
+
+
+def test_show_default():
+    tdata = hist().show()
+    assert type(tdata) == list
